ffe669be8564147ee9fdc69381d1d6f86eb7b946
Andrzej Bartoszek <trevorgryffits@gmail.com>
2013-12-09 20:32:34 +0100

    Random initialization for glowworms

--- SRPP/Optimalization.cs
+++ SRPP/Optimalization.cs
@@ -8,24 +8,45 @@ namespace SRPP
 {
     public class Optimalization
     {
-        public Solution Run(int count)
+        public Solution Run(int count, int max)
         {
             Solution best;
+            Random random = new Random();
             IList<Solution> solutions;
 
-            solutions = Initialize(count);
+            solutions = Initialize(count, max, random);
 
             return null; //hush, compiler
 
         }
 
-        public IList<Solution> Initialize(int count)
+        public IList<Solution> Initialize(int count, int max, Random rnd)
         {
             IList<Solution> solutions = new List<Solution>();
+            IList<int> solution;
 
+            for (int i = 0; i < count; ++i)
+            {
+                solution = Enumerable.Range(1, max).ToList();
+                Shuffle(solution, rnd);
+                solutions.Add(new Solution(solution));
+            }
 
 
             return solutions;
+        }
+
+        private void Shuffle(IList<int> list, Random rnd)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                --n;
+                int k = rnd.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
