539c6a8503324bb058276197ff0c93206bf4ec77
Marcin Copik <mcopik@gmail.com>
2013-12-09 22:54:25 +0100

    Implement parsing args

--- SRPP/Program.cs
+++ SRPP/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NDesk.Options;
+using System.Globalization;
 
 namespace SRPP
 {
@@ -11,35 +13,55 @@ namespace SRPP
     {
         static void Main(string[] args)
         {
-            List<City> readCities = new List<City>();
-            int k;
-            int[] coords;
-            string[] fileLines;
-            List<State> states = new List<State>();
-
-
-            DirectoryInfo di = new DirectoryInfo("basic");
-            var files = di.GetFiles("*", SearchOption.TopDirectoryOnly);
-
-            foreach (FileInfo file in files)
+            double gammaParam = 1.0;
+            double roParam = 0.0;
+            double betaParam = 1.0;
+            double sParam = 1.0;
+            String fileName = null;
+            var p = new OptionSet() {
+            { "g|gamma=", 
+                "gamma parameter",
+              v => {gammaParam = Double.Parse(v,CultureInfo.InvariantCulture);} },
+            { "r|ro=", "r parameter",
+              v => {roParam = Double.Parse(v,CultureInfo.InvariantCulture);} },
+            { "s|sParam=", "s parameter",
+              v => {sParam = Double.Parse(v,CultureInfo.InvariantCulture);} },
+            { "b|beta=", "beta parameter",
+              v => {betaParam = Double.Parse(v,CultureInfo.InvariantCulture);} },
+            };
+            List<String> extra = new List<string>();
+            try
             {
-                readCities.Clear();
-
-                fileLines = File.ReadAllLines(file.FullName);
-
-                k = int.Parse(fileLines[0]);
-                for (int i = 1; i < fileLines.Length; ++i)
+                extra = p.Parse(args);
+                if (extra.Count() == 0)
                 {
-                    coords = fileLines[i].Split(' ').Select(s => int.Parse(s)).ToArray();
-                    readCities.Add(new City(coords[0], coords[1]));
+                    Console.WriteLine("File not given!");
+                    return;
                 }
+                foreach (String str in extra)
+                {
+                    List<City> readCities = new List<City>();
+                    int k = int.Parse(str.Split('k')[1].Substring(1));
+                    Console.WriteLine("Processing file: {0} with parameters k = {1}"
+                                        + " gamma = {2} beta = {3} ro = {4} s = {5}",
+                                        str, k, gammaParam, betaParam, roParam, sParam);
+                    int[] coords;
+                    string[] fileLines;
+                    fileLines = File.ReadAllLines(str);
 
-                states.Add(new State(k, readCities));
+                    for (int i = 1; i < fileLines.Length; ++i)
+                    {
+                        coords = fileLines[i].Split(' ').Select(s => int.Parse(s)).ToArray();
+                        readCities.Add(new City(coords[0], coords[1]));
+                    }
+
+                    State state = new State(k, readCities);
+                }
             }
-
-            Console.WriteLine(states[0].ToString());
-
-
+            catch (OptionException e)
+            {
+                //don't give a fuck
+            }
             Console.ReadLine();
         }
     }
